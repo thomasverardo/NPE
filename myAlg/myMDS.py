ff7cdeb8a72a9fe97899f333ba32ef70402d9950
Thomas <thomas.verardo1@gmail.com>
2023-12-18 15:20:00 +0100

    Add mds and pca from scretch

---
+++ myAlg/myMDS.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def myMDS(D, n_components):
+    
+    n = D.shape[0]
+    
+    # Compute the Gram matrix G
+    G = -0.5 * (D**2)
+
+    # Compute the double centering matrix B
+    H = np.eye(n) - (1/n) * np.ones((n, n))
+    B = np.dot(np.dot(H, G), H)
+    
+    eigvals, eigvecs = np.linalg.eigh(B)
+
+    # Sort the eigenvalues in descending order
+    idx = np.argsort(eigvals)[::-1]
+    eigvals = eigvals[idx]
+    eigvecs = eigvecs[:, idx]
+
+    # Select the k largest eigenvalues and eigenvectors
+    selected_eigvals = eigvals[:n_components]
+    selected_eigvecs = eigvecs[:, :n_components]
+    
+    return selected_eigvecs * np.sqrt(selected_eigvals), eigvals, eigvecs
